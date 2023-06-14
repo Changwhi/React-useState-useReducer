ac3cc6c94c7628b0fb8c239a2f92be13cf619669
Changwhi <whinada@gmail.com>
2023-06-13 21:52:44 -0700

    refactor: apply useContext
    Removed consumer and used useContext hook.
    It allows us to use the context and tap into a context.
    It's simple, easy, and more nicer.

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,37 +1,32 @@
-import React from 'react';
+import React, { useContext }from 'react';
 
 import classes from './Navigation.module.css';
 import AuthContext from '../../context/auth-context';
 
 const Navigation = (props) => {
-    return (
-        <AuthContext.Consumer>
-            {(ctx) => {
-                return (
+    const ctx = useContext(AuthContext);
 
-                    <nav className={classes.nav}>
-                        <ul>
-                            {ctx.isLoggedIn && (
-                                <li>
-                                    <a href="/">Users</a>
-                                </li>
-                            )}
-                            {ctx.isLoggedIn && (
-                                <li>
-                                    <a href="/">Admin</a>
-                                </li>
-                            )}
-                            {ctx.isLoggedIn && (
-                                <li>
-                                    <button onClick={props.onLogout}>Logout</button>
-                                </li>
-                            )}
-                        </ul>
-                    </nav>
-                )
-            }}
-        </AuthContext.Consumer>
-    );
+    return (
+        <nav className={classes.nav}>
+            <ul>
+                {ctx.isLoggedIn && (
+                    <li>
+                        <a href="/">Users</a>
+                    </li>
+                )}
+                {ctx.isLoggedIn && (
+                    <li>
+                        <a href="/">Admin</a>
+                    </li>
+                )}
+                {ctx.isLoggedIn && (
+                    <li>
+                        <button onClick={props.onLogout}>Logout</button>
+                    </li>
+                )}
+            </ul>
+        </nav>
+    )
 };
 
 export default Navigation;
